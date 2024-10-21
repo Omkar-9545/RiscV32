df954bc5ed731cf8069110cd71324919ff4932f6
Omkar Chougule <omkarchougule9545@gmail.com>
2024-10-21 13:43:31 +0530

    Completed evrything testing of processor to be done

--- SingleCycleCPU.v
+++ SingleCycleCPU.v
@@ -30,111 +30,131 @@ module SingleCycleCPU(
 
 // TODO: connect wire to realize SingleCycleCPU
 // The following provides simple template,
+parameter width=32; 
+wire addVal=32'h04;
 
+wire [width-1:0] PC_in,PC_out,addOut,immAddress,rd1,rd2;
+wire [width-1:0] shiftOut,immOut,inst,Operand2,ALUOut,mem_read_data,reg_write_data;
+wire branch,memRead,memtoReg,memWrite,ALUSrc,regWrite,zero,funct7;
+wire [1:0] ALUOp;
+wire [6:0] opcode;
+wire [3:0] ALUCtl;
+wire [2:0] funct3;
+wire [4:0] readReg1,readReg2,writeReg;
 
 PC m_PC(
-    .clk(),
+    .clk(clk),
     .rst(start),
-    .pc_i(),
-    .pc_o()
+    .pc_i(PC_in),
+    .pc_o(PC_out)
 );
 
 Adder m_Adder_1(
-    .a(),
-    .b(),
-    .sum()
+    .a(PC_out),
+    .b(addVal),
+    .sum(addOut)
 );
 
 InstructionMemory m_InstMem(
-    .readAddr(),
-    .inst()
+    .readAddr(PC_out),
+    .inst(inst)
 );
+
+assign opcode = inst[6:0];
 
 Control m_Control(
-    .opcode(),
-    .branch(),
-    .memRead(),
-    .memtoReg(),
-    .ALUOp(),
-    .memWrite(),
-    .ALUSrc(),
-    .regWrite()
+    .opcode(opcode),
+    .branch(branch),
+    .memRead(memRead),
+    .memtoReg(memtoReg),
+    .ALUOp(ALUOp),
+    .memWrite(memWrite),
+    .ALUSrc(ALUSrc),
+    .regWrite(regWrite)
 );
 
+assign readReg1 = inst[19:15];
+assign readReg2 = inst[24:20];
+assign writeReg = inst[11:7];
 
 Register m_Register(
-    .clk(),
+    .clk(clk),
     .rst(start),
-    .regWrite(),
-    .readReg1(),
-    .readReg2(),
-    .writeReg(),
-    .writeData(),
-    .readData1(),
-    .readData2()
+    .regWrite(regWrite),
+    .readReg1(readReg1),
+    .readReg2(readReg2),
+    .writeReg(writeReg),
+    .writeData(reg_write_data),
+    .readData1(rd1),
+    .readData2(rd2)
 );
 
 
 ImmGen #(.Width(32)) m_ImmGen(
-    .inst(),
-    .imm()
+    .inst(inst),
+    .imm(immOut)
 );
 
 ShiftLeftOne m_ShiftLeftOne(
-    .i(),
-    .o()
+    .i(immOut),
+    .o(shiftOut)
 );
 
 Adder m_Adder_2(
-    .a(),
-    .b(),
-    .sum()
+    .a(PC_out),
+    .b(shiftOut),
+    .sum(immAddress)
 );
 
+assign PC_Mux_Sel = branch & zero;
+
 Mux2to1 #(.size(32)) m_Mux_PC(
-    .sel(),
-    .s0(),
-    .s1(),
-    .out()
+    .sel(PC_Mux_Sel),
+    .s0(addOut),
+    .s1(immAddress),
+    .out(PC_in)
 );
 
 Mux2to1 #(.size(32)) m_Mux_ALU(
-    .sel(),
-    .s0(),
-    .s1(),
-    .out()
+    .sel(ALUSrc),
+    .s0(rd2),
+    .s1(immOut),
+    .out(Operand2)
 );
 
+assign funct7 = inst[30];
+assign funct3 = inst[14:12];
+
 ALUCtrl m_ALUCtrl(
-    .ALUOp(),
-    .funct7(),
-    .funct3(),
-    .ALUCtl()
+    .ALUOp(ALUOp),
+    .funct7(funct7),
+    .funct3(funct3),
+    .ALUCtl(ALUCtl)
 );
 
 ALU m_ALU(
-    .ALUctl(),
-    .A(),
-    .B(),
-    .ALUOut(),
-    .zero()
+    .ALUCtl(ALUCtl),
+    .A(rd1),
+    .B(Operand2),
+    .ALUOut(ALUOut),
+    .zero(zero)
 );
 
 DataMemory m_DataMemory(
     .rst(start),
-    .clk(),
-    .memWrite(),
-    .memRead(),
-    .address(),
-    .writeData(),
-    .readData()
+    .clk(clk),
+    .memWrite(memWrite),
+    .memRead(memRead),
+    .address(ALUOut),
+    .writeData(rd2),
+    .readData(mem_read_data)
 );
 
 Mux2to1 #(.size(32)) m_Mux_WriteData(
-    .sel(),
-    .s0(),
-    .s1(),
-    .out()
+    .sel(memtoReg),
+    .s0(ALUOut),
+    .s1(mem_read_data),
+    .out(reg_write_data)
 );
     
 endmodule
